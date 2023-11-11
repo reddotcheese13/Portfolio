1be84359879b363a8282ad5059c5a2236973a5e8
reddotcheese13 <lohitlohi24@gmail.com>
2023-11-11 23:55:53 +0530

    made changes in code

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -6,49 +6,55 @@ import { TypeAnimation } from "react-type-animation";
 const HeroSection = () => {
   return (
     <section>
-      <div className="grid grid-cols-1 sm:grid-cols-12">
+      <div className="grid grid-cols-1 lg:grid-cols-12">
         <div className="col-span-7 place-self-center text-center sm:text-left">
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+            <span className="text-transparent bg-clip-text bg-gradient-to-br from-purple-500 to-pink-600">
               Hello, I&apos;m
             </span>
-            <br />
             <TypeAnimation
               sequence={[
                 "Lohit",
                 1000,
-                "Full Stack developer",
+                "Full Stack Developer",
                 1000,
-                "Programmer",
+                "Java Backend",
+                1000,
+                "Frontend UI/UX",
+                1000,
+                "AEM Developer",
                 1000,
               ]}
               wrapper="span"
               speed={50}
+              style={{ fontSize: "4xl", display: "inline-block" }}
               repeat={Infinity}
             />
           </h1>
-          <p className="text-[#ADB7BE] text-base mb-6 sm:text-lg lg:text-xl ">
+          <p className="text-white text-base sm:text-lg lg:text-xl mb-6">
             Proficient in Java, C++, JavaScript, React, GraphQL, with expertise
             in AEM, Docker, Git, and cloud platforms AWS and Azure, utilizing
             Agile methodologies and specializing in algorithmic domains.
           </p>
           <div>
-            <button className="px-5 py-4 w-full sm:w-fit rounded-full mr-4 bg-white hover:bg-slate-200 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 text-white">
-              Hire me
+            <button className="px-6 py-3 mr-4 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-black">
+              Hire Me
             </button>
-            <button className="px-5 py-4 w-full sm:w-fit rounded-full bg-transparent hover:bg-slate-800 text-white border border-white mt-3 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500">
-              View cv
+            <button className="px-1 py-1 rounded-full w-full sm:w-fit   bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 mt-3 border border-white">
+              <span className="block bg-black hover:bg-slate-600 rounded-full px-5 py-2">
+                Download CV
+              </span>
             </button>
           </div>
         </div>
         <div className="col-span-5 place-self-center mt-4 lg:mt-0">
-          <div className="rounded-full bg-[#181818] w-[400px] h-[400px] lg:w-[450px] lg:h-[450px] relative">
+          <div className="flex items-center justify-center rounded-full bg-[#181818] w-80vw h-80vw md:w-96 md:h-96">
             <Image
-              src="/images/hero-image.png"
+              src="/images/hero-img.png"
               alt="hero image"
-              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              width={300}
-              height={300}
+              className="block rounded-full"
+              width={400} // Adjust the width as needed
+              height={400} // Adjust the height as needed
             />
           </div>
         </div>
