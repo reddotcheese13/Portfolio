538d9f9766252c824e7748456ef959656b77e981
reddotcheese13 <lohitlohi24@gmail.com>
2024-01-13 10:10:36 +0530

    added footer

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import HeroSection from "./components/HeroSection";
 import NavBar from "./components/NavBar";
 import AboutSection from "./components/AboutSection";
 import Projects from "./components/Projects/Projects";
-import Footer from "./components/footer/footer";
+import Footer from "./components/Projects/Footer";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
